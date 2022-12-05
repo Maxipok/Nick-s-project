03e93e361467314d8de327cf0ade3ae140aa3e17
Maxipok <112840575+Maxipok@users.noreply.github.com>
2022-12-04 22:38:22 -0500

    Update game.py

--- game.py
+++ game.py
@@ -19,7 +19,7 @@ HEIGHT = 500
 class Player(pygame.sprite.Sprite):
     # Constructor. Pass in the color of the block,
     # and its x and y position
-    def __init__(self, color, width, height, jumps, is_hidden):
+    def __init__(self, color, width, height):
        # Call the parent class (Sprite) constructor
        pygame.sprite.Sprite.__init__(self)
        # Create an image of the block, and fill it with a color.
@@ -32,18 +32,11 @@ class Player(pygame.sprite.Sprite):
        self.rect = self.image.get_rect()
 
 class Guard(pygame.sprite.Sprite):
-    # Constructor. Pass in the color of the block,
-    # and its x and y position
-    def __init__(self, color, width, height, sees_player, bounces):
-       # Call the parent class (Sprite) constructor
+    def __init__(self, color, width, height):
        pygame.sprite.Sprite.__init__(self)
-       # Create an image of the block, and fill it with a color.
-       # This could also be an image loaded from the disk.
        self.image = pygame.Surface([width, height])
        self.image.fill(color)
 
-       # Fetch the rectangle object that has the dimensions of the image
-       # Update the position of this object by setting the values of rect.x and rect.y
        self.rect = self.image.get_rect()
 
 class Bush(pygame.sprite.Sprite):
@@ -78,26 +71,41 @@ class Popwerup(pygame.sprite.Sprite):
 
 def main():
     pygame.init()
+    jumps = 1
+    is_hidden = False
     level = 1
     size = (WIDTH, HEIGHT)
     screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("Level: %i", level)
+    pygame.display.set_caption(f'Level: {level}')
 
     all_sprites_list = pygame.sprite.Group()
 
-    player = Player(PLAYER_COLOR, 10, 10, 1, False)
-    guard1 = Guard(GUARD_COLOR, 10, 10, False, 0)
-    
+    guards_list = pygame.sprite.Group()
+
+    bushes_list = pygame.sprite.Group()
+
+    player = Player(PLAYER_COLOR, 10, 10)
+    guard1 = Guard(GUARD_COLOR, 10, 10, 0)
+    bush1 = Bush(BUSH_COLOR, 30, 30)
+
     player.rect.x = 450
     player.rect.y = 450
-    
-    guard1.rect.x = random.randint(100, 400)
-    guard1.rect.y = random.randint(100, 400)
+
+    guard1.rect.x = random.randint(50, 350)
+    guard1.rect.y = random.randint(50, 350)
+
+    bush1.rect.x = random.randint(50, 350)
+    bush1.rect.y = random.randint(50, 350)
 
     all_sprites_list.add(player)
     all_sprites_list.add(guard1)
-   
-    
+    all_sprites_list.add(bush1)
+
+    guards_list.add(guard1)
+
+    bushes_list.add(bush1)
+
+
     screen.fill(SURFACE_COLOR)
 
     running = True
@@ -107,50 +115,98 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.K_w and player_rect.y < 500:
-                player_rect.y += 1
-            elif event.type == pygame.K_s and player_rect.y > 0:
-                player_rect.y -= 1
-            elif event.type == pygame.K_d and player_rect.x < 500:
-                player_rect.x += 1
-            elif event.type == pygame.K_a and player_rect.x > 0:
-                player_rect.x -= 1
-            elif event.type == pygame.K_UP and player_rect.y < 470 and info_list[1][2] > 0:
-                player_rect.y += 30
-                info_list[1][2] -= 1
-            elif event.type == pygame.K_DOWN and player_rect.y > 30 and info_list[1][2] > 0:
-                player_rect.y -= 30
-                info_list[1][2] -= 1
-            elif event.type == pygame.K_RIGHT and player_rect.x < 470 and info_list[1][2] > 0:
-                player_rect.x += 30
-                info_list[1][2] -= 1
-            elif event.type == pygame.K_LEFT and player_rect.x > 30 and info_list[1][2] > 0:
-                player_rect.x -= 30
-                info_list[1][2] -= 1
+            elif event.type == pygame.K_w and player.rect.y < 500:
+                player.rect.y += 1
+            elif event.type == pygame.K_s and player.rect.y > 0:
+                player.rect.y -= 1
+            elif event.type == pygame.K_d and player.rect.x < 500:
+                player.rect.x += 1
+            elif event.type == pygame.K_a and player.rect.x > 0:
+                player.rect.x -= 1
+            elif event.type == pygame.K_UP and player.rect.y < 470 and jumps > 0:
+                player.rect.y += 30
+                jumps -= 1
+            elif event.type == pygame.K_DOWN and player.rect.y > 30 and jumps > 0:
+                player.rect.y -= 30
+                jumps -= 1
+            elif event.type == pygame.K_RIGHT and player.rect.x < 470 and jumps > 0:
+                player.rect.x += 30
+                jumps -= 1
+            elif event.type == pygame.K_LEFT and player.rect.x > 30 and jumps > 0:
+                player.rect.x -= 30
+                jumps -= 1
             else:
-                player_rect.x += 0
+                player.rect.x += 0
 
-        all_sprites_list.update()
+        for bush in bushes_list:
+            if pygame.sprite.collide_rect(player, bush):
+                is_hidden = True
+            else:
+                is_hidden = False
+
+        for guard in guards_list:
+            if guard.rect.x > 475 or guard.rect.x < 25:
+                guard.bounces += 1
+
+            if ((abs(guard.rect.x - player.rect.x) < 100) and (abs(guard.rect.y - player.rect.y) < 100) and not is_hidden):
+                if guard.rect.x < player.rect.x and guard.rect.y < player.rect.y:
+                    guard.rect.x += 2
+                    guard.rect.y += 2
+                elif guard.rect.x < player.rect.x and guard.rect.y > player.rect.y:
+                    guard.rect.x += 2
+                    guard.rect.y -= 2
+                elif guard.rect.x > player.rect.x and guard.rect.y < player.rect.y:
+                    guard.rect.x -= 2
+                    guard.rect.y += 2
+                else:
+                    guard.rect.x -= 2
+                    guard.rect.x -= 2
+            else:
+                if guard.bounces % 2 == 0:
+                    guard.rect.x += 1
+                else:
+                    guard.rect.x -= 1
+
+            if pygame.sprite.collide_rect(guard, player):
+                running = False
+
+            if clock % 1000 == 0 and player.jumps == 0:
+                player.jumps += 1
+
+            if player.rect.x < 50 and player.rect.y < 50:
+                level += 1
+
+                new_guard = Guard(GUARD_COLOR, 10, 10, 0)
+                new_guard.rect.x = random.randint(50, 350)
+                new_guard.rect.y = random.randint(50, 350)
+
+                all_sprites_list.add(new_guard)
+                guards_list.add(new_guard)
+
+                if level < 4:
+                    new_bush = Bush(BUSH_COLOR, 30, 30)
+                    new_bush.rect.x = random.randint(50, 350)
+                    new_bush.rect.y = random.randint(50, 350)
+
+                    all_sprites_list.add(new_bush)
+                    bushes_list.add(new_bush)
+
+                for guard in all_sprites_list:
+                    guard.rect.x = random.randint(50, 350)
+                    guard.rect.y = random.randint(50, 350)
+
+                player.rect.x = 450
+                player.rect.y = 450
+
         all_sprites_list.draw(screen)
+        pygame.draw.circle(screen, (0, 0, 255), (25, 25), 50)
         pygame.display.flip()
         clock.tick(60)
 
     pygame.quit()
 
+main()
+
     
     
-Notes:
-    
-    
-    have main function
 
-make config file for the boring stuff (screen size + linking drawings of sprites to pygame)
-
-make classes: 1 for player, enemies, bush, and powerups
-
-classes inherits from pygame Sprite
-
-Screen.blit
-
-
-pygame. Sprite group
