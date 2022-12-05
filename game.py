f4bd9cdbf1223e6cd8d4f553043baf766bc5a7b3
Maxipok <112840575+Maxipok@users.noreply.github.com>
2022-12-04 19:19:56 -0500

    Create game.py

---
+++ game.py
@@ -0,0 +1,106 @@
+import pygame
+import numpy
+import random
+import time
+
+def min(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+PLAYER_COLOR = (0, 0, 0)
+SURFACE_COLOR = (167, 255, 100)
+WIDTH = 500
+HEIGHT = 500
+
+class Player(pygame.sprite.Sprite):
+    # Constructor. Pass in the color of the block,
+    # and its x and y position
+    def __init__(self, color, width, height, jumps, is_hidden):
+       # Call the parent class (Sprite) constructor
+       pygame.sprite.Sprite.__init__(self)
+       # Create an image of the block, and fill it with a color.
+       # This could also be an image loaded from the disk.
+       self.image = pygame.Surface([width, height])
+       self.image.fill(color)
+
+       # Fetch the rectangle object that has the dimensions of the image
+       # Update the position of this object by setting the values of rect.x and rect.y
+       self.rect = self.image.get_rect()
+
+class Guard(pygame.sprite.Sprite):
+    # Constructor. Pass in the color of the block,
+    # and its x and y position
+    def __init__(self, color, width, height, sees_player, bounces):
+       # Call the parent class (Sprite) constructor
+       pygame.sprite.Sprite.__init__(self)
+       # Create an image of the block, and fill it with a color.
+       # This could also be an image loaded from the disk.
+       self.image = pygame.Surface([width, height])
+       self.image.fill(color)
+
+       # Fetch the rectangle object that has the dimensions of the image
+       # Update the position of this object by setting the values of rect.x and rect.y
+       self.rect = self.image.get_rect()
+
+class Bush(pygame.sprite.Sprite):
+    # Constructor. Pass in the color of the block,
+    # and its x and y position
+    def __init__(self, color, width, height):
+       # Call the parent class (Sprite) constructor
+       pygame.sprite.Sprite.__init__(self)
+       # Create an image of the block, and fill it with a color.
+       # This could also be an image loaded from the disk.
+       self.image = pygame.Surface([width, height])
+       self.image.fill(color)
+
+       # Fetch the rectangle object that has the dimensions of the image
+       # Update the position of this object by setting the values of rect.x and rect.y
+       self.rect = self.image.get_rect()
+
+class Popwerup(pygame.sprite.Sprite):
+    # Constructor. Pass in the color of the block,
+    # and its x and y position
+    def __init__(self, color, width, height):
+       # Call the parent class (Sprite) constructor
+       pygame.sprite.Sprite.__init__(self)
+       # Create an image of the block, and fill it with a color.
+       # This could also be an image loaded from the disk.
+       self.image = pygame.Surface([width, height])
+       self.image.fill(color)
+
+       # Fetch the rectangle object that has the dimensions of the image
+       # Update the position of this object by setting the values of rect.x and rect.y
+       self.rect = self.image.get_rect()
+
+def main():
+    level = 1
+    pygame.init()
+    size = (WIDTH, HEIGHT)
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Level: %i", level)
+
+    all_sprites_list = pygame.sprite.Group()
+
+    player = Player(PLAYER_COLOR, 10, 10)
+    player.rect.x = 450
+    player.rect.y = 450
+
+    all_sprites_list.add(player)
+
+    exit = True
+    clock = pygame.time.Clock()
+
+    while exit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit = False
+
+        all_sprites_list.update()
+        screen.fill(SURFACE_COLOR)
+        all_sprites_list.draw(screen)
+        pygame.display.flip()
+        clock.tick(60)
+
+    pygame.quit()
